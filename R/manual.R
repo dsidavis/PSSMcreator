80ac12cfa01d8525ac7666d537b5a893dd0f2d01
Duncan Temple Lang <duncan@r-project.org>
2019-08-01 06:55:48 -0700

    return the results from the manual version

--- R/manual.R
+++ R/manual.R
@@ -1,10 +1,14 @@
-pssm = 
+pssm =
+    # This is the start of doing the computations independently of the GUI.
+    # This is better for testing and ensuring correctness whenever the code is
+    # changed.
+    # And it is easier to use than the GUI.
 function(posf, negf)
 {
     pos = readPosFile(posf)
     neg = readNegFile(negf)
 
-    PSSM = PSSMtable(pos[[2]], negative[[2]])
+    PSSM = PSSMtable(pos[[2]], neg[[2]])
     showPSSM(PSSM, pos[[1]], neg[[1]])
     pscores = score(pos$whole, PSSM = PSSM)
     nscores = score(neg$whole, PSSM = PSSM)
@@ -13,5 +17,11 @@ function(posf, negf)
     jnscores = jackknife.neg(neg$whole, pos$values, neg$values)
 
 
-    unjacked <<- rocdemo.sca(c(rep(1, length(pscores)), rep(0, length(nscores))), c(pscores, nscores), dxrule.sca, caseLabel = "Sulfation", markerLabel = "Unjackknifed Scores")
+    unjacked <- ROC::rocdemo.sca(c(rep(1, length(pscores)), rep(0, length(nscores))), c(pscores, nscores), ROC::dxrule.sca, caseLabel = "Sulfation", markerLabel = "Unjackknifed Scores")
+
+    jacked <- ROC::rocdemo.sca(c(rep(1, length(jpscores)), rep(0, length(jnscores))), c(jpscores, jnscores), ROC::dxrule.sca, caseLabel = "Sulfation", markerLabel = "Jackknifed Scores")
+
+    list(PSSM = PSSM, pscores = pscores, nscores = nscores,
+         jpscores = jpscores, jnscores = jnscores,
+         unjacked = unjacked, jacked = jacked)
 }
